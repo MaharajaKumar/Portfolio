ed01910f41c986c62f0603fbd30d1cc702532622
MaharajaKumar <maharajakumar28@gmail.com>
2024-02-05 11:21:02 +0530

    initial commit

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,101 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "RoomMate React Native",
+    buttonText: "Download",
+    url: "",
+    img: "/roommate.jpg",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 2,
+    title: "Music App React Native",
+    buttonText: "Download",
+    url: "",
+    img: "/music.jpg",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 3,
+    title: "Ecommerce React Native",
+    buttonText: "Download",
+    url: "",
+    img: "/e_commerce_app.webp",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 4,
+    title: "Blog React",
+    buttonText: "See Demo",
+    url: "",
+    img: "/blog.webp",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 5,
+    title: "Ecommerce Next",
+    buttonText: "See Demo",
+    url: "",
+    img: "/e_commerce_web.webp",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>{item.buttonText}</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,144 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={"animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          Connect Today
+          <br />
+          Transforming Ideas into Digital Reality
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <h1>
+            Building{" "}
+            <motion.b whileHover={{ color: "#58D68D" }}>Seamless</motion.b>
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{ color: "#58D68D" }}>
+              Mobile and Web
+            </motion.b>{" "}
+            Solutions.
+          </h1>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <div className="listTitleContainer">
+            <h2>React Native</h2>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: "10px",
+              }}
+            >
+              <img src="/react.png" alt="" />
+              <img src="/expo.png" alt="" />
+            </div>
+          </div>
+
+          <p>
+            I excel in React Native development, JavaScript, Redux/Context APIs,
+            native module integration, UI/UX knowledge, RESTful APIs, and
+            testing frameworks like Jest and React Native Testing Library, with
+            proficient Git version control.
+          </p>
+          {/* <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <div className="listTitleContainer">
+            <h2>React</h2>
+            <img src="/react.png" alt="" />
+          </div>
+          <p>
+            I excel in React.js development, JavaScript, Redux/Context APIs,
+            React Router and Material-UI, RESTful APIs, and testing frameworks
+            like Jest and React Testing Library, with proficient Git version
+            control.
+          </p>
+          {/*  <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <div className="listTitleContainer">
+            <h2>Next</h2>
+            <img src="/next.png" alt="" />
+          </div>
+          <p>
+            I excel in Next.js development, JavaScript, SSR snd SSG(CSR),
+            Redux/Context APIs, Routing with Next.js and React Router,
+            Material-UI and Tailwind CSS, RESTful APIs, MongoDB and Prisma ORM,
+            and testing frameworks like Jest and React Testing Library, with
+            proficient Git version control.
+          </p>
+          {/*  <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <div className="listTitleContainer">
+            <h2>Express</h2>
+            <img src="/express.png" alt="" />
+          </div>
+          <p>
+            I excel in Express.js development, Node.js and JavaScript, RESTful
+            API design principles, middleware integration and route handling,
+            authentication and authorization mechanisms (JWT and OAuth),
+            database integration with MongoDB and MySQL, and Mocha, with
+            proficient Git version control.
+          </p>
+          {/*  <button>Go</button> */}
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
